a438cee3654648d4680e268f0b4bf2a10f336bd1
Yian Zhuang <loydred9@gmail.com>
2024-04-07 14:02:34 -0700

    Created a data set

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        // Use the data here
-        console.log(data); // This will log the array of currencies
+        console.log(data); 
     })
     .catch(error => {
         console.error('Error fetching data:', error);
