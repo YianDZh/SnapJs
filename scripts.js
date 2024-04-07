bb89dc39572bfc33e82e7fc29e988bb948d3fd53
Yian Zhuang <loydred9@gmail.com>
2024-04-07 13:54:50 -0700

    Created a data set

--- scripts.js
+++ scripts.js
@@ -24,8 +24,17 @@
  */
 
 
+fetch('currencies.json')
+    .then(response => response.json())
+    .then(data => {
+        // Use the data here
+        console.log(data); // This will log the array of currencies
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+    const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
